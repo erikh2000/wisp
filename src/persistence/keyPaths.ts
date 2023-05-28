64bdbac81b8a8d3f81263065dc53166c3844247b
erikh2000 <erikrhermansen@Eriks-iMac-2.local>
2023-05-28 12:15:48 -0700

    Fixed warning.

--- src/persistence/keyPaths.ts
+++ src/persistence/keyPaths.ts
@@ -1,7 +1,7 @@
 export const ROOT_PATH = '/';
 export const PROJECTS_PATH = '/projects/';
 export const PROJECT_PATH_TEMPLATE = '/projects/{projectName}';
-export const PROJECT_REGEX_TEMPLATE = '\/projects\/{projectName}\/.*';
+export const PROJECT_REGEX_TEMPLATE = '\\/projects\\/{projectName}\\/.*';
 export const FACES_PATH_TEMPLATE = '/projects/{projectName}/faces/';
 export const FACE_PATH_TEMPLATE = '/projects/{projectName}/faces/{faceName}';
 export const SPIELS_PATH_TEMPLATE = '/projects/{projectName}/spiels/';
