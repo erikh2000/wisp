d36c6ba0e72d5ea627123b052717048793d766fe
erikh2000 <erikrhermansen@Eriks-iMac-2.local>
2023-06-25 13:38:08 -0700

    Fix for "Layout of screen tabs is off."

--- src/ui/screen/NavBarButton.tsx
+++ src/ui/screen/NavBarButton.tsx
@@ -8,11 +8,15 @@ interface IProps {
   isActive:boolean
 }
 
+function _nameToStyle(name:string):string {
+  return styles[name.toLowerCase()] ?? '';
+}
+
 function NavBarButton(props:IProps) {
   const navigate = useNavigate();
   const { name, url, isActive } = props;
   const buttonClasses = isActive ? styles.menuBarButtonActive : styles.menuBarButton;
-  const textClasses = isActive ? styles.menuBarButtonTextActive : styles.menuBarButtonText;
+  const textClasses = `${isActive ? styles.menuBarButtonTextActive : styles.menuBarButtonText} ${_nameToStyle(name)}`;
   return (
     <button className={buttonClasses} onClick={() => { if (!isActive) navigate(url); } }>
       <div className={textClasses}>{name}</div>
